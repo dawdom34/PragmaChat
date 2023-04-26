4548025fd56698df5b7d50b9297cf47ccc7847cc
Dawid <dawdom34@wp.pl>
2023-04-26 20:03:37 +0200

    Added password reset option

--- PragmaChat/urls.py
+++ PragmaChat/urls.py
@@ -37,6 +37,13 @@ urlpatterns = [
         name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_reset/password_change_done.html'), 
         name='password_change_done'),
+    # Password reset
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_done.html'),
+     name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_complete.html'),
+     name='password_reset_complete'),
 
 ]
 if settings.DEBUG:
