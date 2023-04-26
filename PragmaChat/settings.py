8e58c19337f6f882d58fba170abed57dc7af1a95
Dawid <dawdom34@wp.pl>
2023-04-26 12:26:56 +0200

    Home screen view

--- PragmaChat/settings.py
+++ PragmaChat/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Third party apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My apps
+    'personal',
 ]
 
 MIDDLEWARE = [
