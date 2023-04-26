21c46aa9212232f563007eaee20fce773d3ae05a
Dawid <dawdom34@wp.pl>
2023-04-26 14:43:12 +0200

    Added register option

--- PragmaChat/urls.py
+++ PragmaChat/urls.py
@@ -20,9 +20,13 @@ from django.conf.urls.static import static
 
 from personal.views import home_screen_view
 
+from users.views import register_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screen_view, name='home'),
+    # Register new user
+    path('register/', register_view, name='register'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
