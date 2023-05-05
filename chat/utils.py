ecfe81440401e5e51b0b412124b71b78c3e4358f
Dawid <dawdom34@wp.pl>
2023-05-05 13:25:53 +0200

    Added chat activation/deactivation to friend.models.FriendList

---
+++ chat/utils.py
@@ -0,0 +1,16 @@
+from .models import PrivateChatRoom
+
+
+def find_or_create_private_chat(user1, user2):
+	"""
+    Find or create private chat
+    """
+	try:
+		chat = PrivateChatRoom.objects.get(user1=user1, user2=user2)
+	except PrivateChatRoom.DoesNotExist:
+		try:
+			chat = PrivateChatRoom.objects.get(user1=user2, user2=user1)
+		except PrivateChatRoom.DoesNotExist:
+			chat = PrivateChatRoom(user1=user1, user2=user2)
+			chat.save()
+	return
