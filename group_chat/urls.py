30e4c3502adb74d35221402e42dddc6d9fe1fe35
Dawid <dawdom34@wp.pl>
2023-05-26 12:42:08 +0200

    Added backend for group creation

--- group_chat/urls.py
+++ group_chat/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from .views import (group_chat_room_view,
-                    create_group_view)
+                    create_group_view,
+                    create_group)
 
 app_name = 'group_chat'
 
 urlpatterns = [
     path('', group_chat_room_view, name='group-chat-room'),
     path('create_group_view/', create_group_view, name='create-group-view'),
+    path('create_group/', create_group, name='create-group'),
 ]
--- group_chat/views.py
+++ group_chat/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
+from django.http import HttpResponse
+
+from .models import GroupChatRoom
 
 from friend.models import FriendList
+
+from users.models import Account
+
+import json
 
 DEBUG = False
 
@@ -36,3 +43,87 @@ def create_group_view(request):
     context['friends'] = friends
 
     return render(request, 'group_chat/create_group.html', context)
+
+def create_group(request):
+    """
+    Activated by ajax function to create the group with given users
+    """
+    user = request.user
+    payload = {}
+    
+    # Get args from request
+    group_name = request.POST.get('group_name')
+    selected_friends = request.POST.get('selected_friends')
+    
+    # Convert str to list
+    friends = selected_friends.split(',')
+    # Remove last element, it's empty string
+    friends.pop(-1)
+
+    if friends == None:
+        friends = []
+
+    if request.method == 'POST' and user.is_authenticated:
+        # Check if group title is not empty
+        if group_name.strip() != '':
+            # Check if selected users contains at least two users
+            if len(friends) >= 2:
+                # Check if selected users exist
+                friends_valid = True
+                for friend in friends:
+                    try:
+                        user_validate = Account.objects.get(id=int(friend))
+                    except Account.DoesNotExist:
+                        friends_valid = False
+                        payload['response'] = 'One of selected friend is invalid.'
+
+                # Add auth user id to friends list
+                friends.append(user.id)
+
+                group_valid = True
+                # Check if group with the same users already exist
+                # users_group = [[1,3,4], [3], [3,6,7]]
+                users_groups = []
+                for friend in friends:
+                    # Get user object
+                    x = Account.objects.get(id=int(friend))
+                    # Get all groups of witch the user is a part of 
+                    g = GroupChatRoom.objects.filter(users=x)
+                    temp = []
+                    # Append id's to users groups
+                    for y in g:
+                        temp.append(y.id)
+                    users_groups.append(temp)
+
+                common_groups = set(users_groups[0])
+                for s in users_groups[1:]:
+                    common_groups.intersection_update(s)
+
+                for gr in common_groups:
+                    qr = GroupChatRoom.objects.get(id=gr)
+                    qr_users = qr.users.all()
+                    if len(qr_users) == len(friends):
+                        group_valid = False
+                        payload['response'] = 'Group with selected users already exist.'
+
+                if friends_valid and group_valid:
+                    # Create group
+                    group_chat_rom = GroupChatRoom.objects.create(title=group_name, owner=user)
+
+                    # Add selected users to the group
+                    for friend in friends:
+                        person = Account.objects.get(id=int(friend))
+                        group_chat_rom.add_user(person)
+                    
+                    # Add owner to admins group
+                    group_chat_rom.add_admin(user)
+
+                    payload['response'] = 'Group created.'
+            else:
+                payload['response'] = 'You must select at least two friends to create a group.'
+        else:
+            payload['response'] = 'Invalid group name.'
+    else:
+        payload['response'] = 'You must be authenticated to create group.'
+    
+    return HttpResponse(json.dumps(payload))
