5a40370a61c21153add38a2f073abc9896f43bc3
Dawid <dawdom34@wp.pl>
2023-05-26 17:06:16 +0200

    Added the ability to promote users to admins of given group

--- group_chat/urls.py
+++ group_chat/urls.py
@@ -6,7 +6,8 @@ from .views import (group_chat_room_view,
                     edit_group_view,
                     edit_group_title,
                     add_friend_to_group,
-                    remove_friend_from_group)
+                    remove_friend_from_group,
+                    promote_to_admin)
 
 app_name = 'group_chat'
 
@@ -18,4 +19,5 @@ urlpatterns = [
     path('edit_group_title/', edit_group_title, name='edit-group-title'),
     path('add_friend_to_group/', add_friend_to_group, name='add-friend-to-group'),
     path('remove_friend_from_group/', remove_friend_from_group, name='remove-friend-from-group'),
+    path('promote_to_admin', promote_to_admin, name='promote-to-admin'),
 ]
