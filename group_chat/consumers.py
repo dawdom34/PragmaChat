90976bbb3f95c259fb5b2151cb4507a6c7a912f9
Dawid <dawdom34@wp.pl>
2023-05-28 13:32:41 +0200

    Added group info send from backed

--- group_chat/consumers.py
+++ group_chat/consumers.py
@@ -1,5 +1,11 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
+from channels.db import database_sync_to_async
+
+from .models import GroupChatRoom
+
+import json
+
 
 
 class GroupChatConsumer(AsyncJsonWebsocketConsumer):
@@ -34,8 +40,14 @@ class GroupChatConsumer(AsyncJsonWebsocketConsumer):
 				pass
 			elif command == "get_room_chat_messages":
 				pass
-			elif command == "get_user_info":
-				pass
+			elif command == "get_group_info":
+				group = await get_group_or_error(content['room_id'], self.scope['user'])
+				payload = get_group_info(group)
+				if payload != None:
+					payload = json.loads(payload)
+					await self.send_group_info_payload(payload['user_info'])
+				else:
+					raise Exception("Something went wrong retrieving the group details.")
 		except Exception as e:
 			pass
 
@@ -111,3 +123,35 @@ class GroupChatConsumer(AsyncJsonWebsocketConsumer):
 			- Hide the progress bar on UI
 		"""
 		print("DISPLAY PROGRESS BAR: " + str(is_displayed))
+
+	async def send_group_info_payload(self, group_info):
+		"""
+		Send a payload of group information to the ui
+		"""
+		print("GroupChatConsumer: send_group_info_payload. ")
+		await self.send_json({
+			"group_info": group_info,
+		},)
+@database_sync_to_async
+def get_group_or_error(room_id, user):
+	"""
+    Check if room exist and if user is on the users list
+    Returns Group object
+    """
+	try:
+		group = GroupChatRoom.objects.get(id=room_id)
+	except GroupChatRoom.DoesNotExist:
+		raise Exception("Invalid room")
+	
+	if user not in group.users.all():
+		raise Exception("You do not have permissions to join this room.")
+	return group
+
+def get_group_info(group):
+	"""
+    Converts basic group info to json
+    """
+	payload = {}
+	payload['group_id'] = group.id
+	payload['group_title'] = group.title
+	return json.dumps(payload)
